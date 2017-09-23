4b0deda2120bc70d90e096dea4beec51a1ec28db
Justin Ho <justinhodev@gmail.com>
2017-09-23 14:48:40 -0700

    Changed fonts and layout
    +raleway
    +satisfy
    +gradient background

--- site/pages/concept.vue
+++ site/pages/concept.vue
@@ -1,13 +1,10 @@
 <template>
-    <b-jumbotron header="Page under construction" lead="This link is being worked on and will be coming soon" fluid>
-        <p>Visit another page</p>
-        <b-btn variant="primary" to="/">Go back to home</b-btn>
-    </b-jumbotron>
+
 </template>
 
 <script>
 export default {
-
+  layout: 'workInProgress'
 }
 </script>
 
--- site/pages/index.vue
+++ site/pages/index.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-    <b-jumbotron header="Meet the Team" lead="IAT 343">
-      <p>Abe Gupta</p>
-      <p>Ioana Sandor</p>
-      <p>Justin Ho</p>
-      <p>Lucy Huang</p>
-      <p>Sammy Yoo</p>
-    </b-jumbotron>
+    <div class="background"/>
+
+    <b-container class="mt-5 pt-5 pb-5 main">
+      <b-row align-h="center">
+        <b-col cols="10">
+          <h1>Iris</h1>
+        </b-col>
+      </b-row>
+
+      <b-row align-h="center">
+        <b-col cols="9">
+          <h4>Meet the Team</h4>
+        </b-col>
+      </b-row>
+
+      <b-row align-h="center">
+          <b-col cols="9">
+            <ul>
+              <li>Abe Gupta</li>
+              <li>Ioana Sandor</li>
+              <li>Justin Ho</li>
+              <li>Lucy Huang</li>
+              <li>Sammy Yoo</li>
+            </ul>
+          </b-col>
+      </b-row>
+
+    </b-container>
   </div>
 </template>
 
@@ -16,38 +37,49 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
+  },
+  data () {
+    return {
+      title: 'Iris'
+    }
+  },
+  head () {
+    return {
+      title: this.title
+    }
   }
 }
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+body {
+  font-family: 'Raleway', sans-serif;
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.main {
+  color: white;
+  background-color: rgba(225, 225, 225, 0.3);
+  border-radius: 1rem;
+  
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+h1 {
+  font-family: 'Satisfy', cursive;
 }
 
-.links {
-  padding-top: 15px;
+.background {
+  background: #270f36; /* Old browsers */
+background: -moz-linear-gradient(top, #270f36 0%, #642b6b 27%, #c86b98 67%, #f09f9c 80%, #ffc19f 92%, #fd9d7f 100%); /* FF3.6-15 */
+background: -webkit-linear-gradient(top, #270f36 0%,#642b6b 27%,#c86b98 67%,#f09f9c 80%,#ffc19f 92%,#fd9d7f 100%); /* Chrome10-25,Safari5.1-6 */
+background: linear-gradient(to bottom, #270f36 0%,#642b6b 27%,#c86b98 67%,#f09f9c 80%,#ffc19f 92%,#fd9d7f 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#270f36', endColorstr='#fd9d7f',GradientType=0 ); /* IE6-9 */
+
+position: absolute;
+top: 0;
+bottom: 0;
+left: 0;
+right: 0;
+overflow: hidden;
+z-index:-1;
 }
 </style>
