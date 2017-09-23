56a7a9fe6fade0f8e3915ae9d628aaacf14fce0b
Justin Ho <justinhodev@gmail.com>
2017-09-23 14:49:45 -0700

    changed to raleway

--- site/nuxt.config.js
+++ site/nuxt.config.js
@@ -11,7 +11,7 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href:'https://fonts.googleapis.com/css?family=Amatic+SC|Raleway:300'}
+      { rel: 'stylesheet', href:'https://fonts.googleapis.com/css?family=Raleway:300'}, { rel: 'stylesheet', href:'https://fonts.googleapis.com/css?family=Satisfy'}
     ]
   },
   /*
