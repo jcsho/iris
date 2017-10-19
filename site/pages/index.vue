c196b5acaeeb5d0bb2cbaa3597bada4da80acbbb
Justin Ho <justinhodev@gmail.com>
2017-10-18 22:39:43 -0700

    Changed media wrapping to grid layout

--- site/pages/index.vue
+++ site/pages/index.vue
@@ -30,72 +30,88 @@
       </b-row>
 
       <!-- Blog Heading-->
-      <b-row align-h="center" class="mt-4">
+      <b-row align-h="center" class="mt-5">
         <b-col cols="9">
           <h4>Blog</h4>
         </b-col>
       </b-row>
 
       <!-- Blogs -->
-      <b-row align-h="center">
-        <b-col cols="10">
+      <nuxt-link to="/blog/week5">
+        <b-row align-h="center" class="blog py-4">
+          <b-col cols="12" xl="3">
+            <!-- Blog Week 5-->
+            <b-img src="/img/storyboard/3.5.png" fluid />
+          </b-col>
 
-          <!-- Blog Week 5-->
-          <ul class="list-unstyled">
-            <nuxt-link to="/blog/week5">
-              <b-media tag="li" class="px-5 pt-4 pb-4 blog">
-                <b-img slot="aside" src="/img/storyboard/3.5.png" width="240" height="150" />
-                <h5 class="mt-0 mb-1">Week 5 - Storyboard</h5>
-                <p>The storyboard is a culmination of the script and concept art combined to visualize important shots. An animatic is also created to explain the plot at which the shot is showing as well as the mood of the story.</p>
-              </b-media>
-            </nuxt-link>
+          <b-col cols="12" xl="7">
+            <h5 class="mt-0 mb-1">Week 5 - Storyboard</h5>
+            <p>The storyboard is a culmination of the script and concept art combined to visualize important shots. An animatic is also created to explain the plot at which the shot is showing as well as the mood of the story.</p>
+          </b-col>
+        </b-row>
+      </nuxt-link>
 
-            <!-- Blog Week 4-->
-            <nuxt-link to="/blog/week4">
-              <b-media tag="li" class="px-5 pt-4 pb-4 blog">
-                <b-img slot="aside" src="/img/concept/Character Sheet Sketch Colored.jpg" width="240" height="150" />
-                <h5 class="mt-0 mb-1">Week 4 - Character Concept</h5>
-                <p>Iris is portrayed as an innocent girl with a cheerful appearance. These character traits are materialized into Iris' concept with puffy cheeks and petite figure. Inspiration is drawn from similar fairytales to enhance Iris' childish features.</p>
-              </b-media>
-            </nuxt-link>
+      <!-- Blog Week 4-->
+      <nuxt-link to="/blog/week4">
+        <b-row align-h="center" class="blog py-4">
+          <b-col cols="12" xl="3">
+            <b-img src="/img/concept/Character Sheet Sketch Colored.jpg" fluid />
+          </b-col>
 
-            <!-- Blog Week 3-->
-            <nuxt-link to="/blog/week3">
-              <b-media tag="li" class="px-5 pt-4 pb-4 blog">
-                <b-img slot="aside" src="/img/ideation/over-the-garden-wall.jpg" width="240" height="150" />
-                <h5 class="mt-0 mb-1">Week 3 - Environment Concept</h5>
-                <p>The environment concepts are critical in order to convey the correct mood and setting to the audience. Iris will only have two different environments; the two environment concepts are starkly contrasted between a murky and gloomy setting to the dazzling cosmic radiance.</p>
-              </b-media>
-            </nuxt-link>
+          <b-col cols="12" xl="7">
+            <h5 class="mt-0 mb-1">Week 4 - Character Concept</h5>
+            <p>Iris is portrayed as an innocent girl with a cheerful appearance. These character traits are materialized into Iris' concept with puffy cheeks and petite figure. Inspiration is drawn from similar fairytales to enhance Iris' childish features.</p>
+          </b-col>
+        </b-row>
+      </nuxt-link>
 
-            <!-- Blog Week 2-->
-            <nuxt-link to="/blog/week2">
-              <b-media tag="li" class="px-5 pt-4 pb-4 blog">
-                <b-img slot="aside" src="/img/script-blank.png" width="240" height="150" />
-                <h5 class="mt-0 mb-1">Week 2 - Script</h5>
-                <p>The script takes the ideas of the inspirations further by refining the several key moments from the ideation phase. The transition between reality and fantasy, the supernatural cosmic explosions, as well as the peripeteia ending are the key visuals we want to create in the animation.</p>
-              </b-media>
-            </nuxt-link>
+      <!-- Blog Week 3-->
+      <nuxt-link to="/blog/week3">
+        <b-row align-h="center" class="blog py-4">
+          <b-col cols="12" xl="3">
+            <b-img src="/img/ideation/over-the-garden-wall.jpg" fluid />
+          </b-col>
 
-            <!-- Blog Week 1-->
-            <nuxt-link to="/blog/week1">
-              <b-media tag="li" class="px-5 pt-4 pb-4 blog">
-                <b-img slot="aside" src="/img/ideation/steven-universe-starry-sky.png" width="240" height="150" />
-                <h5 class="mt-0 mb-1">Week 1 - Ideation</h5>
-                <p>All good projects begin the creative process by researching background material as well as establishing a general direction to go in. This animation is founded upon the themes of celestial bodies and homonymic use of the word iris. From there we look for reference materials which we can use to create concept artwork.</p>
-              </b-media>
-            </nuxt-link>
-          </ul>
+          <b-col cols="12" xl="7">
+            <h5 class="mt-0 mb-1">Week 3 - Environment Concept</h5>
+            <p>The environment concepts are critical in order to convey the correct mood and setting to the audience. Iris will only have two different environments; the two environment concepts are starkly contrasted between a murky and gloomy setting to the dazzling cosmic radiance.</p>
+          </b-col>
+        </b-row>
+      </nuxt-link>
 
-          <!--<ul>
-                <li>2017-09-27: updated script with quotes, long and short version</li>
-                <li>2017-09-26: new concept art for characters and the setting</li>
-                <li>2017-09-23: basic script outline</li>
-              </ul>-->
-              
-        </b-col>
-      </b-row>
+      <!-- Blog Week 2-->
+      <nuxt-link to="/blog/week2">
+        <b-row align-h="center" class="blog py-4">
+          <b-col cols="12" xl="3">
+            <b-img src="/img/script-blank.png" fluid />
+          </b-col>
 
+          <b-col cols="12" xl="7">
+            <h5 class="mt-0 mb-1">Week 2 - Script</h5>
+            <p>The script takes the ideas of the inspirations further by refining the several key moments from the ideation phase. The transition between reality and fantasy, the supernatural cosmic explosions, as well as the peripeteia ending are the key visuals we want to create in the animation.</p>
+          </b-col>
+        </b-row>
+      </nuxt-link>
+
+      <!-- Blog Week 1-->
+      <nuxt-link to="/blog/week1">
+        <b-row align-h="center" class="blog py-4">
+          <b-col cols="12" xl="3">
+            <b-img src="/img/ideation/steven-universe-starry-sky.png" fluid />
+          </b-col>
+
+          <b-col cols="12" xl="7">
+            <h5 class="mt-0 mb-1">Week 1 - Ideation</h5>
+            <p>All good projects begin the creative process by researching background material as well as establishing a general direction to go in. This animation is founded upon the themes of celestial bodies and homonymic use of the word iris. From there we look for reference materials which we can use to create concept artwork.</p>
+          </b-col>
+        </b-row>
+      </nuxt-link>
+
+      <!--<ul>
+                      <li>2017-09-27: updated script with quotes, long and short version</li>
+                      <li>2017-09-26: new concept art for characters and the setting</li>
+                      <li>2017-09-23: basic script outline</li>
+                    </ul>-->
     </b-container>
   </div>
 </template>
@@ -108,9 +124,9 @@ export default {
 
 <style lang="scss">
 .blog {
-  border-radius:1em;
+  //border-radius: 1em;
   &:hover {
-    background-color:rgba(255, 193, 159, 0.5);
+    background-color: rgba(255, 193, 159, 0.5);
   }
 }
 
